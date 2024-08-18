571026df1adb58670057c2c2a762795a5a85f846
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-08-18 15:03:50 +0900

    show fortune result with small bubble

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,17 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 
 export default function Card({ result, onUpdate, backImageUrl, frontImageUrl }) {
-  // const handleClick = () => onUpdate(result);
   const [imageUrl, setImageUrl] = useState(backImageUrl);
+  const [active, setActice] = useState(false);
   function handleClick() {
     onUpdate(result);
     setImageUrl((url) => (url === backImageUrl ? frontImageUrl : backImageUrl));
+    setActice((boolean) => (boolean ? false : true));
   }
   return (
     <button onClick={handleClick}>
       <img src={imageUrl} alt="カード" width={80} />
+      <span className={active ? "active" : ""}>{result}</span>
     </button>
   );
 }
