87150aaaa6658b197a3658f88af4dfebaf34c2fc
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-08-18 17:27:45 +0900

    refactor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,9 @@
-// import React from "react";
-
 import PropTypes from "prop-types";
 import { useState } from "react";
 
 export default function Card({ result, backImageUrl, frontImageUrl, color, backgroundColor }) {
-  // const [imageUrl, setImageUrl] = useState(backImageUrl);
   const [isButtonActive, setIsButtonActive] = useState(false);
   function handleClick() {
-    // setImageUrl((url) => (url === backImageUrl ? frontImageUrl : backImageUrl));
     setIsButtonActive((boolean) => (boolean ? false : true));
   }
 
